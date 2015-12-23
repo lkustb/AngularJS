d6925507463d46de519eae990dc548be96fb2a2c
Kwen <kliu@inwk.com>
2015-12-23 11:12:03 -0600

    SplitOut_JavaFolder_Comment

---
+++ module1/src/com/valo/newFeature/AnotherClass.java
@@ -0,0 +1,18 @@
+package com.valo.newFeature;
+
+/**
+ * Created by kliu on 12/23/2015.
+ */
+public class AnotherClass {
+
+
+    public static void main(String[] args) {
+        System.out.println("Print from main!");
+    }
+
+
+    private void testAnotherClass() {
+        System.out.println("Test another Class from the Class!");
+    }
+
+}
