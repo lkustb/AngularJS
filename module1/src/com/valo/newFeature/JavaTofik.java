b832a68f3da8f05f5aa42a22d9fa6e18333bc103
lkustb <lk.ie.nwu@gmail.com>
2015-12-23 11:25:07 -0600

    Update JavaTofik.java

--- module1/src/com/valo/newFeature/JavaTofik.java
+++ module1/src/com/valo/newFeature/JavaTofik.java
@@ -7,7 +7,7 @@ public class JavaTofik {
     public static void main(String[] args) {
         System.out.println("This is the test for the version control!");
 
-        System.err.println("This is the first system.err here!");  //
+        System.err.println("This is the first system.err here!---05--");  //
 
     }
 }
