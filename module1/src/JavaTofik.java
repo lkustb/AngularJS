7242f9a71dd8e80c664521fc51eaef1477f695b7
Kwen <kliu@inwk.com>
2015-12-23 10:18:30 -0600

    This is the INWK check-in from IntelliJ

---
+++ module1/src/JavaTofik.java
@@ -0,0 +1,8 @@
+/**
+ * Created by kliu on 12/23/2015.
+ */
+public class JavaTofik {
+    public static void main(String[] args) {
+        System.out.println("This is the test for the version control!");
+    }
+}
