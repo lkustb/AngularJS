e4bed8607f9f4f240b0a5e0de90fc80013e13be2
Kwen <kliu@inwk.com>
2015-12-23 11:11:21 -0600

    VP_test_ticket

--- module1/src/JavaTofik.java
+++ module1/src/JavaTofik.java
@@ -9,8 +9,4 @@ public class JavaTofik {
 
     }
 
-    private void newFeature(){
-
-
-    }
 }
---
+++ module1/src/com/valo/newFeature/TestDelegate.java
@@ -0,0 +1,10 @@
+package com.valo.newFeature;
+
+public class TestDelegate {
+    public TestDelegate() {
+    }
+
+    private void newFeature() {
+        System.out.println("This is the 2nd test here");
+    }
+}
