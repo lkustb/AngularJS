2b379b579cc7bb9ec9c38d4430e28875e9605f30
Kwen <kliu@inwk.com>
2015-12-24 11:24:58 -0600

    Master branch again!

--- module1/src/com/valo/newFeature/TestDelegate.java
+++ module1/src/com/valo/newFeature/TestDelegate.java
@@ -6,5 +6,12 @@ public class TestDelegate {
 
     private void newFeature() {
         System.out.println("This is the 2nd test here");
+
+        System.out.println("This is the 2nd Branch Here!");
+
+        System.out.println("Here is teh Master again!");
+
+
+        System.out.println("This is the HEAD/master test here!");
     }
 }
