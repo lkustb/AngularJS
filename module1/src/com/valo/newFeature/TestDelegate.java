e30df9a5dc3cc709aa8ddacb9a076de24de6866b
Kwen <kliu@inwk.com>
2015-12-24 10:52:34 -0600

    workspace?

--- module1/src/com/valo/newFeature/TestDelegate.java
+++ module1/src/com/valo/newFeature/TestDelegate.java
@@ -6,5 +6,10 @@ public class TestDelegate {
 
     private void newFeature() {
         System.out.println("This is the 2nd test here");
+        System.out.println("Try to create Branch-01");
+
+        System.out.println("Try to create Branch-01----01");
+        System.out.println("Try to create Branch-01----02");
+        System.out.println("Try to create Branch-01----03");
     }
 }
